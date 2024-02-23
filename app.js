80e4b4cc86737927f3dbe5b5227b9cd93c8f73dd
Bassima <93587293+BassimaGH@users.noreply.github.com>
2024-02-23 15:22:57 +0100

    changed the name of the url

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.get('/protected', async(req, res) => {
 //POST REQUESTS
 
 //post new store
-app.post('/api/addStore', express.json(), async(req, res) => {
+app.post('/store/addStore', express.json(), async(req, res) => {
   const store = req.body;
   console.log(store);
   const newStore = await Db.createNewStore(store);
