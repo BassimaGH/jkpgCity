48b8c851f3f408958ec64cd18669e641cfe72ac1
melissamln <melissamelin06@gmail.com>
2024-03-03 18:11:00 +0100

    Created delete functionality

--- public/index.js
+++ public/index.js
@@ -1,27 +1,38 @@
+import { deleteStore } from './delete.js';
 const source = "http://localhost:3001/allStores";
 
 fetch(source)
-  .then((response) => {
-    return response.json();
-  })
-  .then((stores) => {
+  .then(response => response.json())
+  .then(stores => {
     console.log(stores);
     const storesList = document.getElementById("stores-list");
-    stores.forEach((store) => {
+    stores.forEach(store => {
       const storeContainer = document.createElement("div");
       storeContainer.classList.add("store-container");
-      storeContainer.innerHTML = `
-            <h3>${store.name}</h3>
-            <p><strong>URL:</strong> ${store.url}</p>
-          `;
+
+      const storeName = document.createElement("h3");
+      storeName.textContent = store.name;
+      const storeUrl = document.createElement("p");
+      storeUrl.innerHTML = `<strong>URL:</strong> ${store.url}`;
+
+      const deleteBtn = document.createElement("button");
+      deleteBtn.innerText = "Delete";
+      deleteBtn.addEventListener('click', function() {
+        deleteStore(store.name);
+      });
+      
+      storeContainer.appendChild(storeName);
+      storeContainer.appendChild(storeUrl);
+      storeContainer.appendChild(deleteBtn);
       storesList.appendChild(storeContainer);
     });
+
+  
   });
+
 
 function goToCreatePage() {
   window.location.href = "create.html";
 }
 
-document
-  .getElementById("createStoreBtn")
-  .addEventListener("click", goToCreatePage);
+document.getElementById("createStoreBtn").addEventListener("click", goToCreatePage);
