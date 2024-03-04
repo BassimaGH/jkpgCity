b7a11cbca323b249f9622b0b9e0eea48081334f4
Bassima <93587293+BassimaGH@users.noreply.github.com>
2024-03-04 17:35:04 +0100

    fixed the merge issue

--- app.js
+++ app.js
@@ -85,12 +85,8 @@ app.get("/sova", async (req, res) => {
 });
 //////
 //filter
-// app.get("/allStores/:district", async (req, res) => {
-//   const storDistrict = req.params.district;
-
 
 app.delete("/stores/:name", verifyAdmin, async (req, res) => {
-app.delete("/stores/:name", async (req, res) => {
   const { name } = req.params;
   try {
     await Db.deleteStoreById(name);
