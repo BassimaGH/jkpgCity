57e5d776b77c48179ebc29187af114c96d00f726
DinaMostafa00 <modi21tf@student.ju.se>
2024-03-04 18:40:36 +0100

    update

--- public/index.js
+++ public/index.js
@@ -1,12 +1,12 @@
-import { deleteStore } from './delete.js';
+import { deleteStore } from "./delete.js";
 const source = "http://localhost:3001/allStores";
 
 fetch(source)
-  .then(response => response.json())
-  .then(stores => {
+  .then((response) => response.json())
+  .then((stores) => {
     console.log(stores);
     const storesList = document.getElementById("stores-list");
-    stores.forEach(store => {
+    stores.forEach((store) => {
       const storeContainer = document.createElement("div");
       storeContainer.classList.add("store-container");
 
@@ -17,22 +17,27 @@ fetch(source)
 
       const deleteBtn = document.createElement("button");
       deleteBtn.innerText = "Delete";
-      deleteBtn.addEventListener('click', function() {
+      deleteBtn.addEventListener("click", function () {
         deleteStore(store.name);
       });
-      
+
+      const updateButton = document.createElement("button");
+      updateButton.innerText = "Update";
+      updateButton.addEventListener("click", function () {
+        // deleteStore(store.name);
+      });
+
       storeContainer.appendChild(storeName);
       storeContainer.appendChild(storeUrl);
       storeContainer.appendChild(deleteBtn);
       storesList.appendChild(storeContainer);
     });
-
-  
   });
-
 
 function goToCreatePage() {
   window.location.href = "create.html";
 }
 
-document.getElementById("createStoreBtn").addEventListener("click", goToCreatePage);
+document
+  .getElementById("createStoreBtn")
+  .addEventListener("click", goToCreatePage);
